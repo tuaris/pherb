f38e0ae94fdff60783a0082508489f041edeb794
Daniel Morante <daniel@morante.net>
2026-05-20 02:36:07 -0400

    Bump version to 0.1.1

--- system/app.conf.php
+++ system/app.conf.php
@@ -6,7 +6,7 @@
 
 define('APPLICATION_NAME', 'Pherb');
 define('APPLICATION_SLUG', 'pherb');
-define('APPLICATION_VERSION', '0.1.0');
+define('APPLICATION_VERSION', '0.1.1');
 define('APPLICATION_WEBSITE', 'https://github.com/tuaris/pherb');
 
 define('APPLICATION_ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
